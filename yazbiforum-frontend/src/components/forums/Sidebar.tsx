a01b078f77abf76dad79b97c56fdb1f70494be19
= <=>
2024-04-11 21:07:07 +0300

    empty

--- yazbiforum-frontend/src/components/forums/Sidebar.tsx
+++ yazbiforum-frontend/src/components/forums/Sidebar.tsx
@@ -37,18 +37,24 @@ const Sidebar: React.FC<SidebarProps> = ({ setFilterByTags, setFilterByType, fil
       {/* tags */}
       <div className="w-full h-[500px]">
         <div className="w-full flex justify-between">
-          <h3 className="mb-3 text-gray-500">Taglar</h3>
-          <div className="underline text-sm text-gray-500 cursor-pointer" onClick={setFilterAll}>
+          <h3 className="mb-3 text-gray-500 select-none">Taglar</h3>
+          <div className="underline text-sm text-gray-500 cursor-pointer select-none" onClick={setFilterAll}>
             Hepsi
           </div>
         </div>
 
         <div className="w-full h-[45px] rounded-full shadow-md border border-[#eee0c7] flex gap-5 px-1">
           <div className="w-[20%] h-full text-right items-center">
-            <Icon className="mt-[14px] text-gray-400 text-lg" icon_={faSearch} />
+            <Icon className="mt-[14px] text-gray-400 text-lg select-none" icon_={faSearch} />
           </div>
           {/* filter search tags */}
-          <input type="text" placeholder="Ara..." className="w-[80%] h-full px-2 outline-none text-gray-400" onChange={searchTag} style={{ background: "none" }} />
+          <input
+            type="text"
+            placeholder="Ara..."
+            className="w-[80%] h-full select-none px-2 outline-none text-gray-400"
+            onChange={searchTag}
+            style={{ background: "none" }}
+          />
         </div>
 
         <div className="w-full h-[300px] flex flex-wrap gap-2 mt-10">
@@ -57,7 +63,7 @@ const Sidebar: React.FC<SidebarProps> = ({ setFilterByTags, setFilterByType, fil
               {tagsCache?.some((t) => t == tag) ? (
                 <button
                   onClick={() => setFilterByTags(tag)}
-                  className="min-w-10 h-6 border-2 leading-5 border-dotted border-[#3e87fc] bg-[#c3ddfb]  px-3 rounded-md text-xs text-blue-500 cursor-pointer"
+                  className="min-w-10 h-6 border-2 leading-5 border-dotted border-[#3e87fc] select-none bg-[#c3ddfb]  px-3 rounded-md text-xs text-blue-500 cursor-pointer"
                 >
                   {tag}
                 </button>
@@ -65,7 +71,7 @@ const Sidebar: React.FC<SidebarProps> = ({ setFilterByTags, setFilterByType, fil
                 <button
                   disabled
                   onClick={() => setFilterByTags(tag)}
-                  className="min-w-10 h-6 border-2 leading-5 border-dotted border-[#3e87fc] bg-[#c3ddfb]  px-3 rounded-md text-xs text-blue-500 opacity-20"
+                  className="min-w-10 h-6 border-2 leading-5 border-dotted border-[#3e87fc] select-none bg-[#c3ddfb]  px-3 rounded-md text-xs text-blue-500 opacity-20"
                 >
                   {tag}
                 </button>
@@ -77,22 +83,22 @@ const Sidebar: React.FC<SidebarProps> = ({ setFilterByTags, setFilterByType, fil
 
       {/* forum tipleri */}
       <div className="w-full h-[50px]">
-        <h3 className="mb-3 text-gray-500">Forum Tipleri</h3>
+        <h3 className="mb-3 text-gray-500 select-none">Forum Tipleri</h3>
         <div className="w-full flex flex-wrap gap-1">
           <button
-            className="w-[80px] h-6 border-2 rounded-md px-3 text-sm border-dotted border-rose-500 bg-rose-300 text-rose-500"
+            className="w-[80px] h-6 border-2 select-none rounded-md px-3 text-sm border-dotted border-rose-500 bg-rose-300 text-rose-500"
             onClick={() => setFilterByType("soru")}
           >
             Soru
           </button>
           <button
-            className="w-[80px] h-6 border-2 rounded-md px-3 text-sm border-dotted border-rose-500 bg-rose-300 text-rose-500"
+            className="w-[80px] h-6 border-2 select-none rounded-md px-3 text-sm border-dotted border-rose-500 bg-rose-300 text-rose-500"
             onClick={() => setFilterByType("bilgi")}
           >
             Bilgi
           </button>
           <button
-            className="w-[80px] h-6 border-2 rounded-md px-3 text-sm border-dotted border-rose-500 bg-rose-300 text-rose-500"
+            className="w-[80px] h-6 border-2 select-none rounded-md px-3 text-sm border-dotted border-rose-500 bg-rose-300 text-rose-500"
             onClick={() => setFilterByType("tartışma")}
           >
             Tartışma
